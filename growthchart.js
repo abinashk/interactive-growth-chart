ad80e57f9b60b8ef9ddb30b4f1565b4f5cc543f9
abinash <koirala.abinash@gmail.com>
2016-08-21 10:41:33 -0400

    tooltip on top

--- growthchart.js
+++ growthchart.js
@@ -25,7 +25,8 @@ var xscaleticks = 4;
 
 //return goal amount by today
 var goalByToday = function(d){
-    return numType( data.goal * parseDate(d.date).getDate() / new Date(parseDate(d.date).getYear(), parseDate(d.date).getMonth(), 0).getDate());
+    return numType( data.goal * parseDate(d.date).getDate() /
+      new Date(parseDate(d.date).getYear(), parseDate(d.date).getMonth(), 0).getDate());
 };
 
 var getXAxisTickVaues = function(){
@@ -41,23 +42,23 @@ var getCurrentMonthMaxDay = function(){
     return lastDayOfMonth;
 };
 
-var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-];
+var monthNames = [
+                    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+                 ];
 
-function LastDayOfCurrentMonth() {
+var lastDayOfCurrentMonth = function() {
     var today = new Date();
     var lastDayOfMonth = new Date(today.getFullYear(), today.getMonth()+1, 0);
-    // console.log(lastDayOfMonth.toISOString().substring(0, 10));
     return lastDayOfMonth.toISOString().substring(0, 10);
-}
+};
 
-function FirstDayOfCurrentMonth() {
+var firstDayOfCurrentMonth = function() {
     var today = new Date();
     var firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
     // console.log(firstDayOfMonth.toISOString().substring(0, 10));
     return firstDayOfMonth.toISOString().substring(0, 10);
-}
+};
 
 
 //create an SVG
@@ -65,28 +66,12 @@ var svg = d3.select("#graphic").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .style("position", "relative");
 
-var focus = svg.append("g")
-    .style("display", "none");
-
-//import the data
-d3.json("growth_chart_data.json", function(error, json) {
-    if (error) return console.warn(error);
-    data = json;
-    data.goal_growth = [{ "value":0, "date":FirstDayOfCurrentMonth()}, { "value":data.goal, "date":LastDayOfCurrentMonth() }];
-    redraw();
-});
-
-
-//one function to rule them all, but this can be refactored
+// #TODO refactor into functional divisions
 function redraw() {
-
-    // store reference to tooltip to avoid looking it up all the time
-    var simpleTooltip = d3.selectAll("#tooltipContainer")
-        .style("opacity", 0); // hide it since we are done testing now
-
-    //get color codes set for each monthlyu series
+    //get color codes set for each monthly series
     color.domain(d3.keys(data).filter(function(key) { return (key !== "goal"); }));
 
     var linedata = color.domain().map(function(name) {
@@ -100,22 +85,42 @@ function redraw() {
     //x and y scales
     var x = d3.scale.linear()
         .domain([
-        d3.min(linedata, function(c) { return d3.min(c.values, function(v) { return parseDate(v.date).getDate(); }); }),
-        d3.max(linedata, function(c) { return d3.max(c.values, function(v) { return parseDate(v.date).getDate(); }); })
+          d3.min(linedata, function(c) {
+            return d3.min(c.values, function(v) {
+              return parseDate(v.date).getDate();
+            });
+          }),
+          d3.max(linedata, function(c) {
+            return d3.max(c.values, function(v) {
+              return parseDate(v.date).getDate();
+            });
+          })
         ])
         .range([0, width]);
 
     var y = d3.scale.linear()
         .domain([
-        d3.min(linedata, function(c) { return d3.min(c.values, function(v) { return v.value; }); }),
-        d3.max(linedata, function(c) { return d3.max(c.values, function(v) { return v.value; }); })
+          d3.min(linedata, function(c) {
+            return d3.min(c.values, function(v) {
+              return v.value;
+            });
+          }),
+          d3.max(linedata, function(c) {
+            return d3.max(c.values, function(v) {
+              return v.value;
+            });
+          })
         ])
         .range([height, 0]);
 
     //draw lines
     var line = d3.svg.line()
-        .x(function(d) { return x(parseDate(d.date).getDate()); })
-        .y(function(d) { return y(d.value); });
+        .x(function(d) {
+          return x(parseDate(d.date).getDate());
+        })
+        .y(function(d) {
+          return y(d.value);
+        });
 
 
     //create and draw the x axis
@@ -147,15 +152,21 @@ function redraw() {
     //append a g tag for each line and give it a unique ID based on the column name of the data
     var thegraphEnter=thegraph.enter().append("g")
         .attr("class", "thegraph")
-        .attr('id',function(d){ return d.name+"-line"; })
+        .attr('id',function(d){
+          return d.name+"-line";
+        })
         .style("stroke-width",2.5);
 
 
     //append the line to the graph
     thegraphEnter.append("path")
         .attr("class", "line")
-        .style("stroke", function(d) { return color(d.name); })
-        .attr("d", function(d) { return line(d.values[0]); })
+        .style("stroke", function(d) {
+          return color(d.name);
+        })
+        .attr("d", function(d) {
+          return line(d.values[0]);
+        })
         .transition()
         // .duration(2000)
         .attrTween('d',function (d){
@@ -171,41 +182,41 @@ function redraw() {
     var legend = svg.selectAll('.legend')
         .data(linedata);
 
-    var legendEnter=legend
-        .enter()
-        .append('g')
-        .attr('class', 'legend')
-        .attr('id',function(d){ return d.name; })
-        .on('click', function (d) {
-            if($(this).css("opacity") == 1){
-                d3.select(document.getElementById(this.id +"-line"))
-                    .transition()
-                    .style("opacity",0)
-                    .style("display",'none');
-
-                d3.select(this)
-                    .attr('fakeclass', 'fakelegend')
-                    .style ("opacity", 0.2);
-            }
-            else {
-                d3.select(document.getElementById(this.id +"-line"))
-                    .style("display", "block")
-                    .style("opacity",1);
-
-                d3.select(this)
-                    .attr('fakeclass','legend')
-                    .style ("opacity", 1);}
-        });
-
     //create a scale to pass the legend items through
-    var legendscale= d3.scale.ordinal()
-                .domain(lastvalues)
-                .range([0,30,60,90,120,150,180,210]);
+    var legendscale = d3.scale.ordinal()
+                              .domain(lastvalues)
+                              .range([0,30,60,90,120,150,180,210]);
+
+    var legendEnter = legend.enter()
+                            .append('g')
+                            .attr('class', 'legend')
+                            .attr('id',function(d){ return d.name; })
+                            .on('click', function (d) {
+                                if($(this).css("opacity") == 1){
+                                    d3.select(document.getElementById(this.id +"-line"))
+                                        .transition()
+                                        .style("opacity",0)
+                                        .style("display",'none');
+
+                                    d3.select(this)
+                                        .attr('fakeclass', 'fakelegend')
+                                        .style ("opacity", 0.2);
+                                }
+                                else {
+                                    d3.select(document.getElementById(this.id +"-line"))
+                                        .style("display", "block")
+                                        .style("opacity",1);
+                                    d3.select(this)
+                                        .attr('fakeclass','legend')
+                                        .style ("opacity", 1);}
+                            });
 
     //add the circles to the created legend container
     legendEnter.append('circle')
         .attr('cx', width +20)
-        .attr('cy', function(d){return legendscale(d.values[d.values.length-1].value);})
+        .attr('cy', function(d){
+          return legendscale(d.values[d.values.length-1].value);
+        })
         .attr('r', 7)
         .style('fill', function(d) {
             return color(d.name);
@@ -214,8 +225,12 @@ function redraw() {
     //add the legend text
     legendEnter.append('text')
         .attr('x', width+35)
-        .attr('y', function(d){return legendscale(d.values[d.values.length-1].value);})
-        .text(function(d){ return d.name.split("_")[0].capitalizeFirstLetter(); });
+        .attr('y', function(d){
+          return legendscale(d.values[d.values.length-1].value);
+        })
+        .text(function(d){
+          return d.name.split("_")[0].capitalizeFirstLetter();
+        });
 
     //set variable for updating the graph
     var thegraphUpdate = d3.transition(thegraph);
@@ -225,10 +240,13 @@ function redraw() {
 
     legendUpdate.select("circle")
         .attr('cy', function(d, i){
-            return legendscale(d.values[d.values.length-1].value);});
+          return legendscale(d.values[d.values.length-1].value);
+        });
 
     legendUpdate.select("text")
-        .attr('y',  function (d) {return legendscale(d.values[d.values.length-1].value);});
+        .attr('y',  function (d) {
+          return legendscale(d.values[d.values.length-1].value);
+        });
 
 
      //update the axes
@@ -239,6 +257,10 @@ function redraw() {
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
 
+    var focus = svg.append("g")
+            .style("display", "none")
+            .style("position", "relative")
+            .style("z-index", 9999);
 
     wrapper = focus.append("g")
         .attr("class", "wrapper");
@@ -256,8 +278,8 @@ function redraw() {
     tiptext = wrapper.append('foreignObject')
                 .attr('x', 0)
                 .attr('y', 0)
-                .attr('width', 200)
-                .attr('height', 100)
+                .attr('width', 100)
+                .attr('height', 150)
                 .append("xhtml:body");
 
     $.ajax({
@@ -275,8 +297,12 @@ function redraw() {
         .attr("height", height)
         .style("fill", "none")
         .style("pointer-events", "all")
-        .on("mouseover", function() { focus.style("display", null);})
-        .on("mouseout", function() { focus.style("display", "none");})
+        .on("mouseover", function() {
+          focus.style("display", null);
+        })
+        .on("mouseout", function() {
+          focus.style("display", "none");
+        })
         .on("mousemove", mousemove);
 
     //handle mouse movement over the graph event
@@ -288,15 +314,20 @@ function redraw() {
             width0 = -1;
         }
 
-        d3.select("#tooltipTitle").html("Goal By Today:  " + (currencyType(data.goal_growth[1].value * Math.round(x0) / Math.round(x.invert(width)))));
+        d3.select("#tooltipTitle")
+          .html((currencyType(data.goal_growth[1].value * Math.round(x0) /
+            Math.round(x.invert(width))))
+          );
         keys = getDataKeys(data);
         keys.forEach(function (value, i) {
             var x = data[value][Math.round(x0)];
             if (typeof x != 'undefined'){
                 dateObj = parseDate(x.date);
-                dateStr = dateObj.getFullYear() + " " + monthNames[dateObj.getMonth()];
-                d3.select("#tooltipMetricName" + (i+1)).html(dateStr);
-                d3.select("#tooltipMetricValue"+ (i+1)).html(currencyType(x.value));
+                dateStr = monthNames[dateObj.getMonth()];
+                d3.select("#tooltipMetricName" + (i+1))
+                  .html(dateStr);
+                d3.select("#tooltipMetricValue"+ (i+1))
+                  .html(currencyType(x.value));
             }
         });
 
@@ -308,8 +339,22 @@ function redraw() {
             .attr("transform",
                   "translate(" + x(Math.round(x0)) + ")");
 
-        d3.select("#current-total-growth").html(currencyType(data.goal_growth[1].value * Math.round(x0) / Math.round(x.invert(width))));
+        d3.select("#current-total-growth")
+          .html(currencyType(data.goal_growth[1].value * Math.round(x0) /
+            Math.round(x.invert(width))));
     }
 
 //end of the redraw function
 }
+
+
+//import the data
+d3.json("growth_chart_data.json", function(error, json) {
+    if (error) return console.warn(error);
+    data = json;
+    data.goal_growth = [
+                          { "value":0, "date":firstDayOfCurrentMonth()},
+                          { "value":data.goal, "date":lastDayOfCurrentMonth()}
+                        ];
+    redraw();
+});
